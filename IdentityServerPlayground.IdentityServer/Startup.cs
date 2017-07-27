5e7ccc8986ef8617f45423869752438c8e60ac92
Jason Krause <jason.krause@pbcdev.com>
2017-07-26 18:58:55 -0700

    Added Google signon.  Now going to change it to register users and such using the Asp.NET Core Identity framework

--- IdentityServerPlayground.IdentityServer/Startup.cs
+++ IdentityServerPlayground.IdentityServer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -34,6 +35,17 @@ namespace IdentityServerPlayground.IdentityServer
             }
 
             app.UseIdentityServer();
+
+
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                AuthenticationScheme = "Google",
+                DisplayName = "Google",
+                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+
+                ClientId = "393229792315-ghvd3lac46olj96nm59cd9k6o6kh781t.apps.googleusercontent.com",
+                ClientSecret = "9IpXrMqOWXCpg53-8AdDWSlD"
+            });
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
